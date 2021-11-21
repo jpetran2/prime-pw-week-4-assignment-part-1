f5cb08f8afc1519ceb046b513c2a0f11395f1b47
James Petran <jm.ptrn@gmail.com>
2021-11-21 13:32:43 -0600

    implemented helloName function

--- assignment/scripts/function-practice.js
+++ assignment/scripts/function-practice.js
@@ -15,10 +15,11 @@ console.log('Test - should say "Hello World!"', hello());
 // 2. Function to return an personalized hello, using the `name` argument.
 //    for example 'Hello, Jo!', or 'Hello, Stacy!'
 function helloName( name ) {
+  console.log('Hello, ' + name + '!');
   return;
 }
 // Remember to call the function to test
-
+helloName('James');
 
 // 3. Function to add two numbers together & return the result
 function addNumbers( firstNumber ) {
